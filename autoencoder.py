2c94e5752343b327d916cb97a4719fcd58d1d581
Choueri Alexandre <alexandre.choueri@student-cs.fr>
2025-01-10 19:23:58 +0100

    remove added attention to VGAE

--- autoencoder.py
+++ autoencoder.py
@@ -34,43 +34,6 @@ class Decoder(nn.Module):
         return adj
 
 
-
-class DecoderWithAttention(nn.Module):
-    def __init__(self, latent_dim, hidden_dim, n_layers, n_nodes, n_heads=4):
-        super(DecoderWithAttention, self).__init__()
-        self.n_layers = n_layers
-        self.n_nodes = n_nodes
-        self.n_heads = n_heads
-
-        self.self_attention = MultiheadAttention(embed_dim=latent_dim, num_heads=n_heads, batch_first=True)
-        mlp_layers = [nn.Linear(latent_dim, hidden_dim)] + [nn.Linear(hidden_dim, hidden_dim) for _ in range(n_layers - 2)]
-        mlp_layers.append(nn.Linear(hidden_dim, 2 * n_nodes * (n_nodes - 1) // 2))
-
-        self.mlp = nn.ModuleList(mlp_layers)
-        self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        # Add self-attention to refine latent representation
-        x = x.unsqueeze(1)  # Add a sequence length dimension
-        x, _ = self.self_attention(x, x, x)
-        x = x.squeeze(1)  # Remove the sequence length dimension
-
-        for i in range(self.n_layers - 1):
-            x = self.relu(self.mlp[i](x))
-
-        x = self.mlp[self.n_layers - 1](x)
-        x = torch.reshape(x, (x.size(0), -1, 2))
-        x = F.gumbel_softmax(x, tau=1, hard=True)[:, :, 0]
-
-        adj = torch.zeros(x.size(0), self.n_nodes, self.n_nodes, device=x.device)
-        idx = torch.triu_indices(self.n_nodes, self.n_nodes, 1)
-        adj[:, idx[0], idx[1]] = x
-        adj = adj + torch.transpose(adj, 1, 2)
-        return adj
-
-
-
 class GIN(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, latent_dim, n_layers, dropout=0.2):
         super().__init__()
@@ -107,55 +70,6 @@ class GIN(torch.nn.Module):
         out = self.bn(out)
         out = self.fc(out)
         return out
-    
-
-class GINWithAttention(torch.nn.Module):
-    def __init__(self, input_dim, hidden_dim, latent_dim, n_layers, dropout=0.2):
-        super().__init__()
-        self.dropout = dropout
-        
-        self.convs = torch.nn.ModuleList()
-        self.convs.append(GINConv(nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
-            nn.LeakyReLU(0.2),
-            nn.BatchNorm1d(hidden_dim),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.LeakyReLU(0.2)
-        )))
-
-        for layer in range(n_layers - 1):
-            self.convs.append(GINConv(nn.Sequential(
-                nn.Linear(hidden_dim, hidden_dim),
-                nn.LeakyReLU(0.2),
-                nn.BatchNorm1d(hidden_dim),
-                nn.Linear(hidden_dim, hidden_dim),
-                nn.LeakyReLU(0.2)
-            )))
-
-        self.attention = GlobalAttention(
-            nn.Sequential(
-                nn.Linear(hidden_dim, hidden_dim // 2),
-                nn.LeakyReLU(0.2),
-                nn.Linear(hidden_dim // 2, 1),
-                nn.Sigmoid()
-            )
-        )
-
-        self.bn = nn.BatchNorm1d(hidden_dim)
-        self.fc = nn.Linear(hidden_dim, latent_dim)
-
-    def forward(self, data):
-        edge_index = data.edge_index
-        x = data.x
-
-        for conv in self.convs:
-            x = conv(x, edge_index)
-            x = F.dropout(x, self.dropout, training=self.training)
-            
-        out = self.attention(x, data.batch)
-        out = self.bn(out)
-        out = self.fc(out)
-        return out
 
 
 
@@ -169,63 +83,6 @@ class VariationalAutoEncoder(nn.Module):
         self.fc_mu = nn.Linear(hidden_dim_enc, latent_dim)
         self.fc_logvar = nn.Linear(hidden_dim_enc, latent_dim)
         self.decoder = Decoder(latent_dim, hidden_dim_dec, n_layers_dec, n_max_nodes)
-
-    def forward(self, data):
-        x_g = self.encoder(data)
-        mu = self.fc_mu(x_g)
-        logvar = self.fc_logvar(x_g)
-        x_g = self.reparameterize(mu, logvar)
-        adj = self.decoder(x_g)
-        return adj
-
-    def encode(self, data):
-        x_g = self.encoder(data)
-        mu = self.fc_mu(x_g)
-        logvar = self.fc_logvar(x_g)
-        x_g = self.reparameterize(mu, logvar)
-        return x_g
-
-    def reparameterize(self, mu, logvar, eps_scale=1.):
-        if self.training:
-            std = logvar.mul(0.5).exp_()
-            eps = torch.randn_like(std) * eps_scale
-            return eps.mul(std).add_(mu)
-        else:
-            return mu
-
-    def decode(self, mu, logvar):
-       x_g = self.reparameterize(mu, logvar)
-       adj = self.decoder(x_g)
-       return adj
-
-    def decode_mu(self, mu):
-       adj = self.decoder(mu)
-       return adj
-
-    def loss_function(self, data, beta=0.05):
-        x_g  = self.encoder(data)
-        mu = self.fc_mu(x_g)
-        logvar = self.fc_logvar(x_g)
-        x_g = self.reparameterize(mu, logvar)
-        adj = self.decoder(x_g)
-        
-        recon = F.l1_loss(adj, data.A, reduction='mean')
-        kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        loss = recon + beta*kld
-
-        return loss, recon, kld
-
-
-
-class VariationalAutoEncoderWithAttention(nn.Module):
-    def __init__(self, input_dim, hidden_dim_enc, hidden_dim_dec, latent_dim, n_layers_enc, n_layers_dec, n_max_nodes):
-        super(VariationalAutoEncoder, self).__init__()
-        self.n_max_nodes = n_max_nodes
-        self.input_dim = input_dim
-        self.encoder = GINWithAttention(input_dim, hidden_dim_enc, hidden_dim_enc, n_layers_enc)
-        self.fc_mu = nn.Linear(hidden_dim_enc, latent_dim)
-        self.fc_logvar = nn.Linear(hidden_dim_enc, latent_dim)
-        self.decoder = DecoderWithAttention(latent_dim, hidden_dim_dec, n_layers_dec, n_max_nodes)
 
     def forward(self, data):
         x_g = self.encoder(data)
